9050f0b6d4fdd9627da311ad15171b3f1a114cd0
swlee <allmaster1145@gmail.com>
2021-11-26 15:56:32 +0900

    step1

---
+++ 17_atsg.tf
@@ -0,0 +1,18 @@
+resource "aws_placement_group" "pika_pg" {
+  name     = "pika-pg"
+  strategy = "cluster"
+}
+
+resource "aws_autoscaling_group" "pika_atsg" {
+  name                      = "pika-atsg"
+  min_size                  = 2
+  max_size                  = 8
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  force_delete              = true
+  launch_configuration      = aws_launch_configuration.pika_lacf.name
+  placement_group           = aws_placement_group.pika_pg.id
+  vpc_zone_identifier       = [aws_subnet.pika_puba.id, aws_subnet.pika_pubc.id]
+
+}
