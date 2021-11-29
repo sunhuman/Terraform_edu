f23e29d1bc62c04efcd46110100035e466c016c2
swlee <allmaster1145@gmail.com>
2021-11-29 09:12:30 +0900

    step2

--- 17_atsg.tf
+++ 17_atsg.tf
@@ -10,9 +10,9 @@ resource "aws_autoscaling_group" "pika_atsg" {
   health_check_grace_period = 60
   health_check_type         = "EC2"
   desired_capacity          = 2
-  force_delete              = true
+  force_delete              = false
   launch_configuration      = aws_launch_configuration.pika_lacf.name
   #placement_group           = aws_placement_group.pika_pg.id
   vpc_zone_identifier       = [aws_subnet.pika_puba.id, aws_subnet.pika_pubc.id]
-  
+  #target_group_arns = [aws_lb_target_group.pika_albtg.arn]
 }
